c7ff9b53cb934d7d0dc55ae845af64fc3e4951cf
c332030 <c332030@qq.com>
2019-12-26 21:25:17 +0800

    历史 Url 存储在 localStorage

--- src/view/layout/TopView.tsx
+++ src/view/layout/TopView.tsx
@@ -84,7 +84,8 @@ export class TopView extends React.Component<PropTypes, StateTypes> {
 
     // debug(CookieUtils.list());
 
-    const urlsJsonStr = CookieUtils.get(EtcdConstants.COOKIES_URLS);
+    // const urlsJsonStr = CookieUtils.get(EtcdConstants.COOKIES_URLS);
+    const urlsJsonStr = localStorage.getItem(EtcdConstants.COOKIES_URLS);
 
     if(!urlsJsonStr) {
       return;
@@ -163,7 +164,8 @@ export class TopView extends React.Component<PropTypes, StateTypes> {
       urlArr.push(url);
     });
 
-    CookieUtils.set(EtcdConstants.COOKIES_URLS, JSON.stringify(urlArr));
+    localStorage.setItem(EtcdConstants.COOKIES_URLS, JSON.stringify(urlArr));
+    // CookieUtils.set(EtcdConstants.COOKIES_URLS, JSON.stringify(urlArr));
   }
 
   querySearch(host?: string, resolve?: Function) {
